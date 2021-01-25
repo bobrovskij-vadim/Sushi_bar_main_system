186420d0eec0ce4bcfcd13c2490081b60c452218
Vadim <bobrovskij.vadim@gmail.com>
2021-01-25 15:14:04 +0200

    file_upload
    file upload

---
+++ Sushi.py
@@ -0,0 +1,6 @@
+import graphic_interface
+
+
+if __name__ == '__main__':
+
+    main_window = graphic_interface.Graphic_interface() # Запуск основного окна
---
+++ printout.py
@@ -0,0 +1,10 @@
+# Печать заказа
+import tempfile
+import os
+
+
+class Printout:
+    def __init__(self, text_print):
+        filename = tempfile.mktemp(".txt")
+        open(filename, "w").write(text_print)
+        os.startfile(filename, "print")
